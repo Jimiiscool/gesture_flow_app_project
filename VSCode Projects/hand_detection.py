341e08cddcd8e96b4aad6dbddab4e78d99ffaecc
Jimiiscool <jimishodunke@gmail.com>
2026-04-08 19:43:55 -0500

    Script Update: Extract Landmarks From Image and Place Them in a NumPy Array For Future Reference and Use

--- VSCode Projects/hand_detection.py
+++ VSCode Projects/hand_detection.py
@@ -1,5 +1,6 @@
 import mediapipe as mp
 import cv2
+import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -16,8 +17,13 @@ with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) a
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
         if results.multi_hand_landmarks:
-            for hand in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(img, hand, mp_hands.HAND_CONNECTIONS)
+            cords_list = []
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                for num, cords in enumerate(hand_landmarks.landmark):
+                    x_cords = f'X Cords: {cords.x}'
+                    y_cords = f'Y Cords: {cords.y}'
+                    cords_list.append((x_cords, y_cords))
 
         cv2.imshow('Hand Detection', img)
         if cv2.waitKey(10) & 0xFF == ord('q'):
