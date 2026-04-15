67ef62c9baff169d1a4884c80e4224e00c107c5c
Jimiiscool <jimishodunke@gmail.com>
2026-04-14 20:16:24 -0500

    Script Update: Turn Landmark Coordinates Into a Flattened NumPy Array and Extract them Into a CSV File

---
+++ VSCode Projects/detection_practice_project/extract_cords.py
@@ -0,0 +1,14 @@
+import numpy as np
+import csv
+from hand_detection import get_cords
+
+
+data = get_cords()
+
+np.save('hand_cords.npy', data)
+
+with open('C:/Users/ajibo/github_repos/gesture_flow_app_project/data/practice_project_data/hand_cords.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(data)
+
+    print('data.shape')
--- VSCode Projects/detection_practice_project/hand_detection.py
+++ VSCode Projects/detection_practice_project/hand_detection.py
@@ -4,12 +4,17 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-cords_list_array = np.array([]).reshape(-1, 3)
+fingers_up = []
+#num_fingers = []
+#cords_list = []
+hand_array = np.array([])
+cords_list_array = np.array([])
+all_cords = []
 
 
 
-def count_fingers():
-    fingers_up = []
+
+'''def count_fingers():
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             index_finger_tip = hand_landmarks.landmark[8]
@@ -21,46 +26,49 @@ def count_fingers():
             else:
                     fingers_up.append(0)
             result = sum(fingers_up)
-    return result
+    return result'''
+
+def get_cords():
+    all_cords = []
+    cap = cv2.VideoCapture(0)
+    with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img.flags.writeable = False
+            results = hands.process(img)
+            img.flags.writeable = True
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            #img_height, img_width, img_channel = img.shape
 
 
-cap = cv2.VideoCapture(0)
-with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
+            if results.multi_hand_landmarks:
+                
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                    wrist = hand_landmarks.landmark[0]
+                    frame_cords = []
+                    for num, cords in enumerate(hand_landmarks.landmark):
+                        normalized_x = cords.x - wrist.x
+                        normalized_y = cords.y - wrist.y
+                        normalized_z = cords.z - wrist.z
+                        frame_cords.extend([normalized_x, normalized_y, normalized_z])
+                    all_cords.append(frame_cords)
+                    print(frame_cords)
 
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img.flags.writeable = False
-        results = hands.process(img)
-        img.flags.writeable = True
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        img_height, img_width, img_channel = img.shape
+                        
 
-        if results.multi_hand_landmarks:
-            num_fingers = []
-            cords_list = []
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-                wrist = hand_landmarks.landmark[0]
-                result = count_fingers()
-                for num, cords in enumerate(hand_landmarks.landmark):
-                    x_cords = f'X Cords: {cords.x}'
-                    y_cords = f'Y Cords: {cords.y}'
-                    normalized_x = wrist.x - cords.x
-                    normalized_y = wrist.y - cords.y
-                    normalized_z = wrist.z - cords.z
-                    hand_array = np.array([[normalized_x, normalized_y, normalized_z]]).flatten()
-                    cords_list.append((x_cords, y_cords))
-                    num_fingers.append(result)
-                    print(result)
-                    cords_list_array = np.vstack([cords_list_array, hand_array])
-                    
+                        
+            cv2.imshow('Hand Detection', img)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+        cords_list_array = np.array(all_cords)
+        print(cords_list_array.shape)
+        final = cords_list_array
+        cap.release()
+        cv2.destroyAllWindows()
+        return final
+    
 
-                    
-        cv2.imshow('Hand Detection', img)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-print(cords_list_array)
-print(sum(num_fingers))        
-cap.release()
-cv2.destroyAllWindows()
+get_cords()
